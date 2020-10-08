e3c77e7f335d513417e1362c0c1648a566f65bc5
thembajoseph <thembajsph1@gmail.com>
2020-10-08 15:15:10 +0200

    fixed username password

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const pg = require("pg");
 const Pool = pg.Pool
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://thembajoseph:themba17307@localhost:5432/greetingsWebapp';
+const connectionString = process.env.DATABASE_URL || 'postgresql://thembajoseph:themba17307@localhost:5432/greetings-webapp';
 
 const pool = new Pool({
   connectionString
